6638176500c3c41de9794d2f7e190f5d70a0696d
kairiroberto <kairiroberto@gmail.com>
2019-06-12 11:25:06 -0300

    Add files via upload

--- src/main/assets/js/usuario.js
+++ src/main/assets/js/usuario.js
@@ -32,101 +32,57 @@ function validarCPF(cpf) {
 function validarData(data) {
     //alert(data);
     var data = data;
-    var dia = data.substring(8,10)
-    var mes = data.substring(5,7)
-    var ano = data.substring(0,4)
- 
+    var dia = data.substring(8, 10)
+    var mes = data.substring(5, 7)
+    var ano = data.substring(0, 4)
+
     //Criando um objeto Date usando os valores ano, mes e dia.
-    var novaData = new Date(ano, (mes-1) ,dia);
- 
+    var novaData = new Date(ano, (mes - 1), dia);
+
     var mesmoDia = parseInt(dia, 10) == parseInt(novaData.getDate());
     var mesmoMes = parseInt(mes, 10) == parseInt(novaData.getMonth()) + 1;
     var mesmoAno = parseInt(ano) == parseInt(novaData.getFullYear());
- 
+
     if ((mesmoDia) && (mesmoMes) && (mesmoAno)) {
         return true;
-    }  
+    }
     return false;
 }
 
-function validarCampos(cpf, nascimento, nome, email, telefone, endereco, cidade) {
+function validarCampos(imei, cpf, nascimento, nome, email, telefone, endereco, cidade) {
     if (!validarCPF(cpf)) {
         alert("CPF inválido.");
+        //showAndroidToast("CPF inválido.");
         return false;
     }
     if (!validarData(nascimento)) {
-        alert('Data inválida.');   
+        alert('Data inválida.');
+        //showAndroidToast("Data inválida.");
         return false;
     }
     if (nome == '' || email == '' || telefone == '' || endereco == '' || cidade == '') {
         alert("Campos vazios.");
+        //showAndroidToast("Campos vazios.");
+        return false;
+    }
+    if (imei == null || imei == "") {
+        alert("Campo imei vazio");
         return false;
     }
     if (nome.length < 8) {
         alert("Nome do usuário muito curto.");
+        //showAndroidToast("Nome do usuário muito curto.");
         return false;
     }
     return true;
 }
 
-function add() {
-    var email = document.getElementById("email").value;
-    var telefone = document.getElementById("telefone").value;
-    var nome = document.getElementById("nome").value;
-    var cpf = document.getElementById("cpf").value;
-    var nascimento = document.getElementById("nascimento").value;
-    var endereco = document.getElementById("endereco").value;
-    var cidade = document.getElementById("cidade").value;
-
-    if (validarCampos(cpf, nascimento, nome, email, telefone, endereco, cidade)) {
-
-        var firebaseConfig = {
-            apiKey: "AIzaSyDyw6S4YRYPPRmLiL6DKvHtZnDrqUlzwAU",
-            authDomain: "arraia-ea992.firebaseapp.com",
-            databaseURL: "https://arraia-ea992.firebaseio.com",
-            projectId: "arraia-ea992",
-            storageBucket: "arraia-ea992.appspot.com",
-            messagingSenderId: "212604244048",
-            appId: "1:212604244048:web:b4f33e67ee59faf4"
-        };
-        // Initialize Firebase
-        firebase.initializeApp(firebaseConfig);
-        var id = firebase.database().ref().child('usuario').push().key;
-        firebase.database().ref('usuario/' + id).set({
-            nome: nome,
-            email: email,
-            telefone: telefone,
-            cpf: cpf,
-            nascimento: nascimento,
-            endereco: endereco,
-            cidade: cidade
-        }).then(function () {
-            alert("Dados salvos com sucesso.");
-            window.location.href = "create.html?mensagem=OK";
-        }).catch(function (error) {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            alert("Erro: " + errorCode + ": " + errorMessage);
-            window.location.href = "create.html?erro=" + errorCode + "-" + errorMessage;
-        });
-
+function validar(usuario) {
+    if (validarCampos(usuario.imei, usuario.cpf, usuario.nascimento, usuario.nome, usuario.email, usuario.telefone, usuario.endereco, usuario.cidade)) {
+        return true;
     } else {
-        alert("Faça a correção do(s) campo(s)!");
+        var msg = "Faça a correção do(s) campo(s)!";
+        alert(msg);
+        return false;
     }
-}
-
-function edit() {
-    
-}
-
-function findId(id) {
-    
-}
-
-function findCPF(cpf) {
-    
-}
-
-function find() {
-    
 }
